2aeddd1eca869a920429b629f2a7d8ec2c2fec2a
Esteban Vergara Giraldo <Teteban0917@gmail.com>
2022-08-19 04:03:31 -0500

    Add files via upload

--- src/results/Metro_Pathfinder.py
+++ src/results/Metro_Pathfinder.py
@@ -1,85 +1,254 @@
-import time, pandas as pd, os, networkx as nx, folium, webbrowser as wb
+''' IMPORTANT. It is necessary to install PANDAS, NETWORKX, and FOLIUM from CMD with PIP '''
+#      Example: [Using CMD] "C:\Users\username> pip install pandas"
+import pandas as pd     # [DATAFRAME]. transforms the .csv to a pandas [DATAFRAME]
+import networkx as nx   #  [DIJKSTRA]. transforms the dataframe to a GRAPH, to apply a [DIJKSTRA] pathfinder algorithm on it
+import folium           #      [PLOT]. converts dijkstra's path into to something [PLOTTABLE], and returns an HTML
+import os               #      [SAVE]. to [SAVE] that html
+import webbrowser as wb #      [OPEN]. to [OPEN] that html in a browser
+import time             #      [WAIT]. the execution [WAITS] for a bit so it's comfortable to see the important info
+from math import dist   #  [DISTANCE]. quick calculation of [DISTANCE] between points
 
-dataframe = pd.read_csv("streets_database.csv", sep=';')
-dataframe.head()
+
+
+# Reads the database and saves it into a pandas dataframe for its use
+# name | origin | destination | length | oneway | geometry
+dataframe = pd.read_csv("streets_database.csv", sep=';') 
+# Transforms the pandas dataframe into a graph. The streets are now nodes with  between coordinates
 graph = nx.from_pandas_edgelist(dataframe,source='origin',target='destination',edge_attr= 'length')
 
-def pathfinding(graph , selected_origin : tuple,selected_destination : tuple) -> list :
+
+
+# Graphicates the shortest path between two points in a web map
+def PATHFINDING(selected_origin : tuple,selected_destination : tuple) -> list :
+
+    print(f'\n--> ORIGIN:  {selected_origin}')
+    print(f'--> DESTINY: {selected_destination}\n')
+    time.sleep(3)
+
+    # Dijkstra's pathfinder alghorithm. Searchs the shortest route between two nodes of our graph
     djk= nx.dijkstra_path(graph, source= str(selected_origin), target= str(selected_destination), weight= 'length')
     length = nx.dijkstra_path_length(graph,str(selected_origin),str(selected_destination),'length\n')
 
     print(f'--> ROUTE:\n{djk}\n')
-    print(f'--> LENGTH: {int(length)} meters since {selected_origin} to {selected_destination}\n')
+    print(f'--> LENGTH: {int(length*100)} meters since {selected_origin} to {selected_destination}\n')
 
-    #folium uses the (lat, long) format, not our (long, lat) format, so...
+    # Folium uses the (lat, long) format, not our (long, lat) format, so...
     for i in range(len(djk)):   djk[i] = eval(djk[i])[::-1]
 
-    #map's zoom and center
-    map = folium.Map([(selected_origin[1]+selected_destination[1])/2 , (selected_origin[0]+selected_destination[0])/2], zoom_start = "14")
-    route = folium.PolyLine(djk,color = 'red' ,weight = 10,opacity = 0.8).add_to(map)
+    # Map's initial zoom and center
+    map = folium.Map([sum(selected_origin[1],selected_destination[1])/2 , sum(selected_origin[0],selected_destination[0])/2], zoom_start = "14")
+    # Line config
+    route = folium.PolyLine(djk,color = 'red' ,weight = 5,opacity = 0.8).add_to(map)
 
-    #saves the map and the render config in a html 
+    # Saves the map and the render config in a html 
     map.save (os.path.join('Route.html'))
 
-    #opens the html in your browser
+    # Opens the html in your default browser
     wb.open_new_tab('Route.html')
-    #now you can see the map!
+    # Now you can see the map!
 
-    #returns this, the coordinates list, for future uses if someone needs it.
+    # Returns the coordinates list for future uses
     return djk
 
-# ubications menu EXAMPLE. Please use a dict() for the coordinates return in next uses 
+
+
+# Numeric menu to select the points for your route. Redirects you to other sub-menus
 def ubication_selection_menu() -> tuple :
-    print("1.EAFIT University            6.Coltejer Building          11.Robledo      ")
-    print("2.Medellín's  University      7.San Antonio Station        12.Laureles     ")
-    print("3.Antioquia's  University     8.Explora Park               13.San Cristóbal")
-    print("4.Nacional University         9.St. Atanasio Girardot      14.Santo Domingo")
-    print("5.Luis Amigó University      10.Aeropuerto O. Herrera      15.Prado        ")
-    print("                     >> 0. UBICACIÓN PERSONALIZADA <<")
+    print("1. LINE A (Niquia - La Estrella)")
+    print("2. LINE B (San Javier - San Antonio)")
+    print("3. LINE T (San Antonio - Oriente)")
+    print("0. CHOOSE A CUSTOM COORDINATE")
 
-    #selected number invalid cases
-    try: num = int(input())
-    except:
-        print('Please select a valid ubication.')
-        time.sleep(4)
-        return ubication_selection_menu()
-    if num not in range(0,16): 
-        print('Please select a valid ubication.')
-        time.sleep(4)
-        return ubication_selection_menu()
+    # Selected number option
+    while True:
+        option = input('>> ').lower()
+        if option.isnumeric():
+            option = int(option)
+        # We have numeric options, but we can use char options for the lines too
+        if option in list(range(0,4))+['a','b','t']:
+            break    
+        else:
+            print('Please select a valid option')
 
-    if num == 0:
-        coords = input()
-        if coords[0] != "(": coords = "("+coords+")"
-        return eval(coords) if coords[1]=="-" else eval(coords)[::-1]
+    if option == '0': return custom_coord_menu()
+    if option in [1,'a']: return coords_selection_line_A()
+    if option in [2,'b']: return coords_selection_line_B()
+    if option in [3,'t']: return coords_selection_line_T()
 
-    if num == 1: return (-75.578416, 6.2007688)
-    if num == 2: return (-75.6101004, 6.2312125)
-    if num == 3: return (-75.5694416, 6.2650137)
-    if num == 4: return (-75.5762232, 6.266327)
-    if num == 5: return (-75.5832559, 6.2601878)
 
-    if num == 6: return (-75.5664549, 6.2500233)
-    if num == 7: return (-75.5697559, 6.2472846)
-    if num == 8: return (-75.5635812, 6.2703451)
-    if num == 9: return (-75.5879543, 6.2568545)
-    if num == 10: return (-75.5887149, 6.216229)
 
-    if num == 11: return (-75.583682, 6.2892842)
-    if num == 12: return (-75.6123571, 6.2440634)
-    if num == 13: return (-75.6371302, 6.2793696)
-    if num == 14: return (-75.5387441, 6.2986144)
-    if num == 15: return (-75.5575788, 6.2596423)
+# Sub-menu
+def coords_selection_line_A():
+    print("\n-> LINE A:")
+    print("1. Niquia             8. Hospital            15. Poblado   ")
+    print("2. Bello              9. Prado               16. Aguacatala  ")
+    print("3. Madera            10. Parque Berrío       17. Ayurá       ")
+    print("4. Acevedo           11. San Antonio         18. Envigado    ")
+    print("5. Tricentenario     12. Alpujarra           19. Itagui      ")
+    print("6. Caribe            13. Exposiciones        20. Sabaneta    ")
+    print("7. Universidad       14. Industriales        21. La Estrella ")
 
-# python main (yup, looks rare but python have a 'main' format)
+    coords_line_A = {1: (-75.5571114, 6.3115868), 2: (-75.5571114, 6.3115868), 3: (-75.5571114, 6.3115868), 4: (-75.5589453, 6.2990036), 5: (-75.5642928, 6.2908133), 6: (-75.5696692, 6.2773731), 7: (-75.565927, 6.2695386), 8: (-75.5636321, 6.2639618), 9: (-75.5661495, 6.2567282), 10: (-75.5682139, 6.2503631), 11: (-75.569809, 6.2473028), 12: (-75.5721487, 6.2432105), 13: (-75.5732653, 6.2383899), 14: (-75.575271, 6.2301564), 15: (-75.578089, 6.2125688), 16: (-75.5814861, 6.19359), 17: (-75.5828842, 6.1884255), 18: (-75.5931765, 6.1931561), 19: (-75.5948746, 6.1935344), 20: (-75.6355661, 6.1751822), 21: (-75.6454255, 6.1654827)}
+
+    while True:
+        option = input('>> ')
+        if option in map(str,range(1,22)):
+            option = int(option)
+            break    
+        else:
+            print('Please select a valid option')
+
+    return coords_line_A[option]
+    
+
+
+# Sub-menu
+def coords_selection_line_B():
+    print("\n-> LINE B:")
+    print("1. San Javier        4. Estadio              7. San Antonio  ")
+    print("2. Santa Lucía       5. Suramericana         -               ")
+    print("3. Floresta          6. Cisneros             -               ")
+
+    coords_line_B = {1: (-75.6136328, 6.2567588), 2: (-75.6037893, 6.2581547), 3: (-75.5978493, 6.2585128), 4: (-75.5884782, 6.2533936), 5: (-75.5827968, 6.2528555), 6: (-75.574962, 6.2506914), 7: (-75.569809, 6.2473028)}
+
+    while True:
+        option = input('>> ')
+        if option in map(str,range(1,8)):
+            option = int(option)
+            break    
+        else:
+            print('Please select a valid option')
+
+    return coords_line_B[option]
+
+
+
+# Sub-menu
+def coords_selection_line_T():
+    print("\n-> LINE T (Trainway):")
+    print("1. San Antonio       4. Bicentenario         7. Loyola               ")
+    print("2. San José          5. Buenos Aires         8. Alejandro Echavarria ")
+    print("3. Pabellón EPM      6. Miraflores           9. Oriente              ")
+
+    coords_line_T = {1: (-75.569809, 6.2473028), 2: (-75.5656191, 6.2474828), 3: (-75.5617771, 6.2454194), 4: (-75.5589926, 6.2440992), 5: (-75.5535271, 6.2412797), 6: (-75.548963, 6.2416769), 7: (-75.5451867, 6.2392961), 8: (-75.5418453, 6.2358023), 9: (-75.5405103, 6.2334242)}
+
+    while True:
+        option = input('>> ')
+        if option in map(str,range(1,8)):
+            option = int(option)
+            break    
+        else:
+            print('Please select a valid option')
+
+    return coords_line_T[option]
+
+
+
+# You also can use a different coordinate for your route, a custom one, you just need to write them
+def custom_coord_menu():
+    while True:
+        print('Write your own coords!')
+        original_coords = input('>> ')
+
+        original_coords = original_coords.replace('(','').replace(')','').replace(',',' ').replace('/',' ').split()
+        original_coords = tuple(sorted(map(eval,original_coords)))
+
+        # If the distance between the custom coord and the Medellin's center is too high,
+        # the custom point is so far from medellín that it is better to choose a different one.
+        if dist(original_coords,(-75.574236, 6.259676)) > 2.5:
+            print('The specified coordinates are not in Medellín.\n--> Try again')
+            continue
+        else:
+            break
+
+    final_coords = approximator(original_coords)
+
+    print(f'{original_coords} was approximated to {final_coords}')
+    time.sleep(3)
+
+    return final_coords
+
+
+
+# Approximates the custom coords to one of the coords we already have in the dataframe, now we can use it.
+def approximator(original_coords):
+    # A list with all the streets origin nodes in tuple type, we will iterate in it
+    origins = map(eval,set(list(dataframe['origin'])))
+    aprox_coords = None     #coord
+    distance = None         #distance to compare    
+    
+    for i in origins:
+        distance_temp = dist(original_coords,i)
+
+        if aprox_coords != None:
+            if distance_temp < distance:
+                aprox_coords = i
+                distance =  distance_temp
+        else:
+            aprox_coords = i
+            distance = distance_temp
+
+    return aprox_coords
+
+
+
+# MAIN (yup, looks weird but python have a 'main' format)
 if __name__ == '__main__':
-    repeat_pathfinding = True
+
+    #You can select if you want to execute the path research for multiple locations or not
+    repeat_pathfinding = True  # while aux
     while repeat_pathfinding == True:
+
+        print('\n-> Select an ORIGIN to your route\n')
         selected_origin = ubication_selection_menu()
+
+        print('\n-> Select the DESTINY of your route\n')
         selected_destination = ubication_selection_menu()
-        pathfinding(graph, selected_origin, selected_destination)
+
+        # Graphicates the route with the selected ubications,
+        # the shortest path between that two coordinates.
+        PATHFINDING(selected_origin, selected_destination)
+
+        # You can search routes multiple times if you want to
         print('Do you want to search another route?\nY/N: ', end = '')
-        repeat_input = input()
-        if repeat_input.lower() in ['n','no',0]:
-            repeat_pathfinding = False
-    print('\nGoodbye!')
+        while True:
+    
+            repeat_input = input()
+
+            if repeat_input.lower() in ['n','no','0']:
+                repeat_pathfinding = False
+                break # The pathfinding ends.
+
+            elif repeat_input.lower() in ['y','yes','s','si','1']:
+                break # The pathfinding cicle continues.
+
+            # [Invalid input case]
+            else:
+                print("Please select Yes or No")
+                continue # The input cicle continues until a valid option is reached.
+
+    print('\nGoodbye!') # And that's all c:
+
+
+
+#               INDEX:    PATHFINDING                  22
+#                         ubication_selection_menu     56
+#                         coords_selection_line_A      81
+#                         coords_selection_line_B     106
+#                         coords_selection_line_T     127
+#                         custom_coord_menu           148 
+#                         approximator                174
+#                         main                        196
+#
+
+
+'''
+    _                ___       _.--.    
+    \`.|\..----...-'`   `-._.-'_.-'`   
+    /  ' `         ,       _.-'       
+    )/' _/     \   `-_,   /           
+    `-'" `"\_  ,_.-;_.-\_ ',     
+        _.-'_./   E_.'   ; /       
+       {_.-``-'         C_/     
+
+'''
